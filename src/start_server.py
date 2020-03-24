88591b1b7cbbfb33f50d0e844933aaafb76fecb8
ssofronov <ssofronov@hedgeserv.com>
2020-03-24 16:25:27 +0200

    Update hello message

--- src/start_server.py
+++ src/start_server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from our demo app with a change and another change!"
+    return "Hello from our demo app!"
 
 
 if __name__ == '__main__':
